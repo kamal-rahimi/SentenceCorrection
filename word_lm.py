1d09e1338cb3ba2e222d3cfaddc5a2af339bff07
Kamal Rahimi Malekshan <rahimi.kamal@gmail.com>
2019-01-22 22:13:05 -0500

    Initial commit - Added the main structure

---
+++ word_lm.py
@@ -0,0 +1,46 @@
+"""
+Word based longuage model
+"""
+
+def load_text(filename):
+    """
+    """
+    pass
+
+def clean_text(data):
+    """
+    """
+    pass
+
+def prepare_data(cleaned_data):
+    """
+    """
+    pass
+
+def create_model():
+    """
+    """
+    pass
+
+def train_model():
+    """
+    """
+    pass
+
+def evaluate_model():
+    """
+    """
+    pass
+
+
+def main():
+    filename = ""
+    data = load_text(filename)
+    cleaned_data = clean_text(data)
+    prepare_data(cleaned_data) #     x_train, y_train, x_test, y_test
+    create_model()
+    train_model()
+    evaluate_model()
+
+if __name__ == '__main__':
+    main()
