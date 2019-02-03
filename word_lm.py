3e87687763acc1258d5bf229d1b5900ec5a69132
Kamal Rahimi Malekshan <rahimi.kamal@gmail.com>
2019-02-03 17:22:21 -0500

    Added model training and a method to gernate randon stences based on the longuage model.

--- word_lm.py
+++ word_lm.py
@@ -4,11 +4,17 @@ Word based longuage model
 
 import argparse
 from nltk import tokenize
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Embedding, LSTM, Dense, Activation
 from collections import Counter
 import string
 import numpy as np
+
+import os
+
+from numpy.random import choice
+
+from keras.preprocessing.sequence import pad_sequences
 
 def load_text(file_path):
     """
@@ -35,7 +41,7 @@ def clean_text(raw_text):
     cleaned_text = ' '.join(token_words)
     return cleaned_text
 
-def create_vocabulary(text, max_vocab_size = 400):
+def create_vocabulary(text, max_vocab_size = 2000):
     """ Create Vocabulary dictionary
     Args:
         text(str)
@@ -46,14 +52,14 @@ def create_vocabulary(text, max_vocab_size = 400):
     """
     words = text.split()
     freq = Counter(words)
-    word2id = {'<PAD>' : 0, '<UNK>' : 1, '<EOS>' : 2}
-    id2word = {0 : '<PAD>', 1 : '<UNK>', 2 : '<EOS>'}
+    word2id = {'<PAD>' : 0, '<BGN>' : 1, '<UNK>' : 2, '<EOS>' : 3}
+    id2word = {0 : '<PAD>', 1 : '<BGN>', 2 : '<UNK>', 3 : '<EOS>'}
 
     for word, _ in freq.most_common():
         id = len(word2id)
         word2id[word] = id
         id2word[id] = word
-        if id > max_vocab_size:
+        if id > max_vocab_size - 4:
             break
 #    with open("temp.txt", 'w') as f:
 #        for key, val in word2id.items():
@@ -87,28 +93,40 @@ def strip_punctuation(text):
     clean_text = text.translate(translator)
     return clean_text
 
-def prepare_data(word2id, token_sentences, max_sentence_length = 15 ):
-    """
+def prepare_data(word2id, token_sentences, max_sentence_words = 12 ):
+    """Prepares dataset for a longuage model
+    Args:
+        word2id: dictionary to convert from words to id
+        token_sentences: a python array of senetnces
+        max_sentence_words: maximim senetnce length
+    Return:
+        X: sequences of words
+        y: Next word in each sequnce
     """
     data = []
     for sentence in token_sentences:
         sentence = strip_punctuation(sentence)
-        token_words = sentence.split()
-        print(token_words)
-        size = min( len(token_words), max_sentence_length - 1 )
-        senetence_words_padded = ['<PAD>']*(max_sentence_length - size) + token_words + ['<EOS>']
-        #print(senetence_words_padded)
-        senetence_words_id_padded = [word2id[word] if word in word2id else word2id['<UNK>'] for word in senetence_words_padded]
-        #senetence_words_id_padded = [word2id[word] for word in senetence_words_padded]
-        data.append(senetence_words_id_padded)
+        sentence_token_words = sentence.split()
+        sentence_token_words = ['<BGN>'] + sentence_token_words + ['<EOS>']
+        senetnce_size = min(len(sentence_token_words), max_sentence_words)
+    #    print(token_words)
+        for word_size in range(2, senetnce_size+1):
+            token_words = sentence_token_words[: word_size]
+            num_pads = max_sentence_words - word_size
+            token_words_padded =  ['<PAD>']*num_pads + token_words
+            #print(token_words_padded)
+            token_words_padded_id = [word2id[word] if word in word2id else word2id['<UNK>'] for word in token_words_padded]
+            #senetence_words_id_padded = [word2id[word] for word in senetence_words_padded]
+            data.append(token_words_padded_id)
 
-    print(data)
-
-    return np.array(data)
+    #print(data)
+    data = np.array(data)
+    X = data[:, :-1]
+    y = data[:,-1]
+    return X, y
 
 def create_model(vocab_size, embedding_dim=40):
-    """
-    Create modelself.
+    """Create keras model
     Args:
         vocabulary vocab_size
         embedding dimmestion
@@ -117,21 +135,65 @@ def create_model(vocab_size, embedding_dim=40):
     """
     model = Sequential([
         Embedding(input_dim=vocab_size, output_dim=embedding_dim, mask_zero=True),
-        LSTM(70),
+        LSTM(70, return_sequences=False),
         Dense(vocab_size),
         Activation('softmax'),
     ])
     return model
 
-def train_model():
+def train_model(model, X, y):
+    """ Trains the keras model
+    Args:
+        model: sequential model
+        X: train dataset
+        y: train labels
+    Return:
+        model: trained model
+    """
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='Nadam',
+                  metrics=['accuracy'])
+    model.fit(X, y, epochs=100, verbose=2)
+    return model
+
+def evaluate_model(model):
     """
     """
     pass
 
-def evaluate_model():
+def generate_text(model, word2id, id2word, vocab_size, max_sentence_words = 12, num_sentences = 1):
+    """ generates new senetnces based on longuage models
+    Args:
+        max_sentence_words: maximum number of words in a senetnce
+        num_sentences: number of sentnces to be genarted
+
+    Return:
+        text: a paython list of genrated senetnces
     """
-    """
-    pass
+    text = []
+    for _ in range(num_sentences):
+        seq = [word2id['<BGN>']]
+        for i in range(12):
+            x = pad_sequences([seq], maxlen=11, truncating='pre')
+            y = model.predict_classes(x, verbose=0)
+            y_prob = model.predict(x, verbose=0)
+            p = y_prob.reshape(-1,1)[:,0]
+            a = np.array(range(0,vocab_size)).reshape(-1,1)[:,0]
+            if i == 0:
+                predict_word = choice(a, p=p)
+            else:
+                for k in y:
+                    if k != word2id['<UNK>']:
+                        predict_word = k
+
+            seq.append(predict_word)
+            if predict_word == word2id['<EOS>']:
+                break
+        new_words = [id2word[id] for id in seq]
+        senetnce = ' '.join(new_words)
+        text.append(senetnce)
+        print(senetnce + '\n')
+    return text
 
 
 def main():
@@ -146,13 +208,23 @@ def main():
     data = load_text(data_path)
 
     cleaned_data = clean_text(data)
-    word2id, id2word = create_vocabulary(cleaned_data)
-    tokenize_sentence(cleaned_data)
-    prepare_data(word2id, ["this is a test", "Second sentence"]) #     x_train, y_train, x_test, y_test
-    vocab_size=2000
-    create_model(vocab_size)
-    train_model()
-    evaluate_model()
+    vocab_size = 10000
+    word2id, id2word = create_vocabulary(cleaned_data, vocab_size)
+    token_senetnces = tokenize_sentence(cleaned_data)
+    print(len(token_senetnces))
+    max_sentence_words = 12
+    X, y = prepare_data(word2id, token_senetnces[:16000], max_sentence_words) #     x_train, y_train, x_test, y_test
+
+    if os.path.isfile("word_lm.h5") == False:
+        model = create_model(vocab_size)
+        model.summary()
+        model = train_model(model, X, y)
+        model.save('word_lm.h5')
+        evaluate_model(model)
+    else:
+        model = load_model('word_lm.h5')
+
+    text = generate_text(model, word2id, id2word, vocab_size, max_sentence_words, 10)
 
 if __name__ == '__main__':
     main()
